ffc71ba0815cb346b19f8c71c30ecded95df178f
Dominick Modica <DLM15@dowling.edu>
2015-04-27 03:43:56 -0400

    First Commit
    One small step for man. One giant leap for myself-kind.

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -0,0 +1,163 @@
+package com.bignerdranch.android.geoquiz;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class QuizActivity extends ActionBarActivity {
+    private int userScore=0;
+    private Boolean UserAnswer;
+    private Button mPrevButton;
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+    private TextView mUserScore;
+
+    private TrueFalse[] mQuestionBank = new TrueFalse[] {
+            new TrueFalse("Electrons are larger than molecules.",false),
+            new TrueFalse("The Atlantic Ocean is the biggest ocean on Earth.",false),
+            new TrueFalse("The chemical make up food often changes when you cook it.",true),
+            new TrueFalse("Sharks are mammals.",false),
+            new TrueFalse("The human body has four lungs.",false),
+            new TrueFalse("Atoms are most stable when their outer shells are full.",true),
+            new TrueFalse("Filtration separates mixtures based upon their particle size",true),
+            new TrueFalse("Venus is the closest planet to the Sun.",false),
+            new TrueFalse("Conductors have low resistance.",true),
+            new TrueFalse("Molecules can have atoms from more than one chemical element.",true),
+            new TrueFalse("Water is an example of a chemical element.",false),
+            new TrueFalse("The study of plants is known as botany.",true),
+            new TrueFalse("Mount Kilimanjaro is the tallest mountain in the world.",false),
+            new TrueFalse("Floatation separates mixtures based on density",true),
+            new TrueFalse("Herbivores eat meat.",false),
+            new TrueFalse("Atomic bombs work by atomic fission.",true),
+            new TrueFalse("Molecules are chemically bonded.",true),
+            new TrueFalse("Spiders have six legs.",false),
+            new TrueFalse("Kelvin is a measure of temperature.",true),
+            new TrueFalse("The human skeleton is made up of less than 100 bones.",false)
+
+    };
+    //set current question index to 0
+    private int mCurrentIndex = 0;
+
+    //helper function to update questions
+    private void updateQuestionNext(){
+        mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+        String question = mQuestionBank[mCurrentIndex].getQuestion();
+        mQuestionTextView.setText(question); }
+    //helper function to cycle through questions
+    private void updateQuestionPrevious(){
+        mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+        String question = mQuestionBank[mCurrentIndex].getQuestion();
+        mQuestionTextView.setText(question);
+
+    }
+    //handles logic for comparing user response to the actual answer
+    private Boolean IsAnswerCorrect(Boolean UserAnswer){
+        Boolean CorrectAnswer=mQuestionBank[mCurrentIndex].isTrueQuestion();
+        return UserAnswer == CorrectAnswer;
+    }
+    //handles logic for updating player score in the event  their response is correct
+    private void gainPoints(){
+        userScore+=1;
+        mUserScore.setText(Integer.toString(userScore));
+        Toast.makeText(QuizActivity.this,R.string.correct_response,Toast.LENGTH_SHORT).show();
+        updateQuestionNext();
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+        mUserScore=(TextView)findViewById(R.id.playerScore);
+        mUserScore.setText(Integer.toString(userScore));
+        mQuestionTextView=(TextView)findViewById(R.id.question_text_view);
+        String question = mQuestionBank[mCurrentIndex].getQuestion();
+        mQuestionTextView.setText(question);
+
+        //user answers question with True
+        mTrueButton=(Button)findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(IsAnswerCorrect(true))
+                    gainPoints();
+                else
+                {
+                    Toast.makeText(QuizActivity.this,R.string.incorrect_response,Toast.LENGTH_SHORT).show();
+                    updateQuestionNext();
+                }
+            }
+        });
+
+
+        mFalseButton=(Button)findViewById(R.id.false_button);
+        //user answers question with False
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(IsAnswerCorrect(false))
+                {
+
+                    gainPoints();
+                }
+                else
+                {
+                    Toast.makeText(QuizActivity.this,R.string.incorrect_response,Toast.LENGTH_SHORT).show();
+                    updateQuestionNext();
+                }
+
+            }
+        });
+        mNextButton=(Button)findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                updateQuestionNext();
+
+
+            }
+        });
+        mPrevButton=(Button)findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                updateQuestionPrevious();
+            }
+        });
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_quiz, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/TrueFalse.java
@@ -0,0 +1,22 @@
+package com.bignerdranch.android.geoquiz;
+
+/**
+ * Created by dom on 4/20/2015.
+ */
+public class TrueFalse {
+
+    private String mQuestion;
+    private Boolean mTrueQuestion;
+    public TrueFalse (String question, boolean trueQuestion){
+        mQuestion=question;
+        mTrueQuestion=trueQuestion;
+    }
+
+    public String getQuestion(){return mQuestion;}
+    public void setQuestion(String question){mQuestion=question;}
+    public boolean isTrueQuestion(){return mTrueQuestion;}
+
+    public void setTrueQuestion(Boolean trueQuestion) {
+        mTrueQuestion = trueQuestion;
+    }
+}
